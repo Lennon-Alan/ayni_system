5dbef22c42fad1e75a7eace801585c6b6cbb4ba0
Lennon-Alan <jhonalexchsucasaire@gmail.com>
2025-08-27 19:40:30 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cq+jb(xn!^+6^%ja+bg8=w73=xnf)ty*sr7uemwc73epqovorj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ayni-system.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
